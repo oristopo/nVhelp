8e88ded543e53d1c0d1d78c9f8dfcdcd30f93f77
Oristo Po <ormpoa@gmail.com>
2019-10-27 20:53:20 -0400

    match nanoVNA display

--- html/screenV.py
+++ html/screenV.py
@@ -37,8 +37,8 @@ if 2 > len(sys.argv) :
 cmd = "capture\r"
 ser = open()
 ser.write(cmd.encode())
-# create a gamma LUT while nanoVNA churns
-lut = np.array(np.rint(255*np.float_power(np.arange(256)/255,1/2.4)), dtype=np.uint32)
+# while NanoVNA churns, create a gamma LUT to better match LCD
+lut = np.array(np.rint(255*np.float_power(np.arange(256)/255,1.4)), dtype=np.uint32)
 
 # need to prune leading bytes
 prune = 9
@@ -48,24 +48,26 @@ b = ser.read(blen)
 #print(len(b))
 #print(b[0:prune])
 #print(b.find(b"\n"))
-#recalculate prune and blen based on newline
+# Recalculate prune and blen based on newline
 #prune = 1 + b.find(b"\n")
 #blen = prune + 320 * 240 * 2
+
 # unlikely to grab entire screen buffer in one read()
 while (blen > (len(b))):
     sleep(0.1)
     b += ser.read(blen)
 
-# prune leading and trailing
-# ">" for big-endian 16-bit
-x = struct.unpack("<76800H", b[prune:blen])
+# prune leading and trailing buffer:  b[prune:blen]
+# ">" for big-endian, "H" for 16-bit
+# This endian is WRONG but matches colors sent to nanoVNA LCD..
+x = struct.unpack(">76800H", b[prune:blen])
 
-# unpack uint16 to uint32
+# pack uint16 to uint32
 arr = np.array(x, dtype=np.uint32)
 # convert pixel format 565(RGB) to 8888(RGBA)
-# while A (alpha) is unused, numpy does not directly support 24-bit color values
+# while A (alpha) is unused, numpy cannot directly support 24-bit color values
 # could in theory use stride_tricks https://stackoverflow.com/a/34128171
-arr = 0xFF000000 + (lut[(arr & 0xF800) >> 8]<<16) + (lut[(arr & 0x07E0) >> 3]<<8) + lut[(arr & 0x001F) << 3]
+arr = 0xFF000000 + (lut[(arr & 0xF800) >> 8]) + (lut[(arr & 0x07E0) >> 3]<<8)  + (lut[(arr & 0x001F) << 3]<<16)
 
 # wants alpha as most significant byte
 img = Image.frombuffer('RGBA', (320, 240), arr, 'raw', 'RGBA', 0, 1)
